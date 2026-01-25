a7a014d1c2c97bd1b772d9b123cf516224c7926b
khanglmfx63155-prog <khanglmfx63155@funix.edu.vn>
2026-01-25 14:33:42 +0700

    create index.html

---
+++ script.js
@@ -0,0 +1,41 @@
+function showDangKy() {
+  document.getElementById("divdangnhap").style.display = "none";
+  document.getElementById("divdangky").style.display = "block";
+}
+
+function showDangNhap() {
+  document.getElementById("divdangky").style.display = "none";
+  document.getElementById("divdangnhap").style.display = "block";
+}
+
+const inputPass = document.querySelector("#pass");
+const checkboxShow = document.querySelector("#show");
+
+checkboxShow.addEventListener("change", function () {
+  inputPass.type = this.checked ? "text" : "password";
+});
+
+// function showMatKhau() {
+//   document.querySelector("#show").onclick = function () {
+//     let inputPass = document.querySelector("#pass");
+//     let checkboxShow = document.getElementById("show");
+//     if (checkboxShow.checked && inputPass.type == "password") {
+//       inputPass.type = "text";
+//     } else {
+//       inputPass.type = "password";
+//     }
+//   };
+// }
+
+const btn = document.getElementById("Submit");
+
+function checkLogIn() {
+  if (user === "" || pass === "") {
+    alert("Vui lòng nhập đầy đủ Username và Password!");
+  } else {
+    alert("Đăng nhập thành công!");
+    window.location.href = "gioithieubanthan.html";
+  }
+}
+
+btn.addEventListener("click", checkLogIn);
